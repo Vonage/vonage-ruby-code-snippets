bbdd05dd6223a2bdd137eb00d7aed7056e338361
leggetter <phil@leggetter.co.uk>
2017-02-15 22:58:11 +0000

    Use key and secret in constructor

--- voice/outbound_tts_call.rb
+++ voice/outbound_tts_call.rb
@@ -3,21 +3,27 @@ require 'nexmo'
 
 Dotenv.load
 
+API_KEY = ENV['API_KEY']
+API_SECRET = ENV['API_SECRET']
 APPLICATION_ID = ENV['APPLICATION_ID']
 PRIVATE_KEY_PATH = ENV['PRIVATE_KEY_PATH']
 PRIVATE_KEY = File.read(PRIVATE_KEY_PATH)
 FROM_NUMBER = ENV['FROM_NUMBER']
-TO_NUMBER = ENV['FROM_NUMBER']
+TO_NUMBER = ENV['TO_NUMBER']
 
 client = Nexmo::Client.new(
+  key: API_KEY,
+  secret: API_SECRET,
   application_id: APPLICATION_ID,
   private_key: PRIVATE_KEY
 )
 
-client.create_call(
+response = client.create_call(
   from: { type: 'phone', number: FROM_NUMBER },
   to: [{ type: 'phone', number: TO_NUMBER }],
   answer_url: [
     'https://nexmo-community.github.io/ncco-examples/first_call_talk.json'
   ]
 )
+
+puts response
