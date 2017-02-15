e5a30448092645c7f392d33d4ec948260336b0f3
Adam Butler <adam@lab.io>
2017-02-15 11:54:48 +0000

    split out env vars

--- voice/outbound_tts_call.rb
+++ voice/outbound_tts_call.rb
@@ -3,13 +3,18 @@ require 'nexmo'
 
 Dotenv.load
 
+APPLICATION_ID = ENV['APPLICATION_ID']
+PRIVATE_KEY_PATH = ENV['PRIVATE_KEY_PATH']
+FROM_NUMBER = ENV['FROM_NUMBER']
+TO_NUMBER = ENV['FROM_NUMBER']
+
 client = Nexmo::Client.new(
-  application_id: ENV['APPLICATION_ID'],
-  private_key: File.read(ENV['PRIVATE_KEY_PATH']),
+  application_id: APPLICATION_ID,
+  private_key: File.read(PRIVATE_KEY_PATH),
 )
 
 client.create_call(
-  from: { type: 'phone', number: ENV['FROM_NUMBER'] },
-  to: [{ type: 'phone', number: ENV['TO_NUMBER'] }],
+  from: { type: 'phone', number: FROM_NUMBER },
+  to: [{ type: 'phone', number: TO_NUMBER }],
   answer_url: ['https://nexmo-community.github.io/ncco-examples/first_call_talk.json']
 )
