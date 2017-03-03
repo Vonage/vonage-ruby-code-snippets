00f5354df546fef89bf487f2bb952d3c833c9861
leggetter <phil@leggetter.co.uk>
2017-03-03 17:51:06 +0000

    Updates for new Voice landing page

--- voice/outbound_tts_call.rb
+++ voice/outbound_tts_call.rb
@@ -1,21 +1,21 @@
 require 'dotenv'
-require 'nexmo'
 
 Dotenv.load
 
 API_KEY = ENV['API_KEY']
 API_SECRET = ENV['API_SECRET']
 APPLICATION_ID = ENV['APPLICATION_ID']
-PRIVATE_KEY_PATH = ENV['PRIVATE_KEY_PATH']
-PRIVATE_KEY = File.read(PRIVATE_KEY_PATH)
+PRIVATE_KEY = ENV['PRIVATE_KEY']
 FROM_NUMBER = ENV['FROM_NUMBER']
 TO_NUMBER = ENV['TO_NUMBER']
+
+require 'nexmo'
 
 client = Nexmo::Client.new(
   key: API_KEY,
   secret: API_SECRET,
   application_id: APPLICATION_ID,
-  private_key: PRIVATE_KEY
+  private_key: File.read(PRIVATE_KEY)
 )
 
 response = client.create_call(
