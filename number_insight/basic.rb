9b483213fd62901447d470324ed0a794ced6674b
Chris Guzman <guzmantchris@gmail.com>
2017-06-12 00:09:28 -0500

    Add number insight examples
    Basic, Standard, and Advanced examples

---
+++ number_insight/basic.rb
@@ -0,0 +1,22 @@
+require 'dotenv'
+require 'nexmo'
+
+Dotenv.load
+
+API_KEY = ENV['API_KEY']
+API_SECRET = ENV['API_SECRET']
+
+puts "Enter the number to lookup, leading with the country code."
+
+number = gets.chomp
+
+client = Nexmo::Client.new(
+  key: API_KEY,
+  secret: API_SECRET
+)
+
+response = client.get_basic_number_insight(
+  number: number
+)
+
+puts response
