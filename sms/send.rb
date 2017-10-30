85a91ba550a6fc372c59a0a6be136de4f894b148
Adam Butler <adam@lab.io>
2017-10-30 10:17:50 +0000

    Update SMS & Voice examples

--- sms/send.rb
+++ sms/send.rb
@@ -1,22 +1,20 @@
 require 'dotenv'
 Dotenv.load
 
-API_KEY = ENV['API_KEY']
-API_SECRET = ENV['API_SECRET']
-FROM_NUMBER = ENV['FROM_NUMBER']
+NEXMO_API_KEY = ENV['NEXMO_API_KEY']
+NEXMO_API_SECRET = ENV['NEXMO_API_SECRET']
+NEXMO_NUMBER = ENV['NEXMO_NUMBER']
 TO_NUMBER = ENV['TO_NUMBER']
 
 require 'nexmo'
 
 client = Nexmo::Client.new(
-  key: API_KEY,
-  secret: API_SECRET
+  key: NEXMO_API_KEY,
+  secret: NEXMO_API_SECRET
 )
 
-response = client.send_message(
-  from: FROM_NUMBER,
+client.send_message(
+  from: NEXMO_NUMBER,
   to: TO_NUMBER,
   text: 'Hello from Nexmo!'
 )
-
-puts response
