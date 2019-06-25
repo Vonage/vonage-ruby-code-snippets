cec75fe0929f3e6129352d41e64195219aa21e4c
benhayehudi <bengreenberg@gmail.com>
2019-06-25 08:29:59 +0300

    added remaining ruby voice snippets

---
+++ voice/make-outbound-call-with-ncco.rb
@@ -0,0 +1,30 @@
+require 'dotenv'
+
+Dotenv.load
+
+NEXMO_APPLICATION_ID = ENV['NEXMO_APPLICATION_ID']
+NEXMO_APPLICATION_PRIVATE_KEY_PATH = ENV['NEXMO_APPLICATION_PRIVATE_KEY_PATH']
+
+require 'nexmo'
+
+client = Nexmo::Client.new(
+    application_id: NEXMO_APPLICATION_ID,
+    private_key: File.read(NEXMO_APPLICATION_PRIVATE_KEY_PATH)
+)
+
+client.calls.create(
+    to: [{
+        type: 'phone',
+        number: RECIPIENT_NUMBER
+    }],
+    from: {
+        type: 'phone',
+        number: NEXMO_NUMBER
+    },
+    answer_url: ['ncco' => 
+        [
+            'action' => 'talk',
+            'text' => 'This is a text to speech call from Nexmo'
+        ] 
+    ]
+)
---
+++ voice/track-ncco-progress.rb
@@ -0,0 +1,44 @@
+require 'sinatra'
+require 'sinatra/multi_route'
+require 'json'
+
+YOUR_URL = 'put your externally accessible url here'
+
+before do
+    content_type :json
+end
+
+route :get, :post, '/webhooks/answer' do
+    [
+        [
+            'action' => 'talk',
+            'text' => 'Thanks for calling the notification line'
+        ],
+        [
+            'action' => 'notify',
+            'payload' => ['foo' => 'bar'],
+            'eventUrl' => ["#{YOUR_URL}/webhooks/notifications"]
+        ],
+        [
+            'action' => 'talk',
+            'text' => 'You will never hear me as the notification URL will return an NCCO'
+        ]
+    ].to_json
+end
+
+route :get, :post, '/webhooks/notifications' do
+    puts params
+    [
+        [
+            'action' => 'talk',
+            'text' => 'Your notification has been received, loud and clear'
+        ]
+    ].to_json
+end
+
+route :get, :post, 'webhooks/event' do
+    puts params
+    halt 204
+end
+
+set :port, 3000
