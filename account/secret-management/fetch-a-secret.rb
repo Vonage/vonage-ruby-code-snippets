ffbd59ec1b866db4fafe3c7f319ecc72bbacf2aa
benhayehudi <bengreenberg@gmail.com>
2019-07-05 13:28:05 +0300

    refactored fetch a secret snippet to accurate data retrieval

--- account/secret-management/fetch-a-secret.rb
+++ account/secret-management/fetch-a-secret.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 require 'dotenv'
+
 Dotenv.load
 
 NEXMO_API_KEY = ENV['NEXMO_API_KEY']
@@ -9,11 +12,9 @@ NEXMO_SECRET_ID = ENV['NEXMO_SECRET_ID']
 require 'nexmo'
 
 client = Nexmo::Client.new(
-    api_key: NEXMO_API_KEY,
-    api_secret: NEXMO_API_SECRET
+  api_key: NEXMO_API_KEY,
+  api_secret: NEXMO_API_SECRET
 )
-
 result = client.secrets.get(NEXMO_SECRET_ID)
-result_hash = result._embedded.secrets[0].to_h
 
-puts "ID: #{result_hash[:id]} created on #{result_hash[:created_at]}"
+puts "ID: #{result.id} created on #{result.created_at}"
