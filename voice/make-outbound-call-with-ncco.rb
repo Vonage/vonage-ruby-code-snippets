391169a139ce0f752f850368717500a556683f09
benhayehudi <bengreenberg@gmail.com>
2019-07-09 09:58:07 +0300

    rubocop fixes

--- voice/make-outbound-call-with-ncco.rb
+++ voice/make-outbound-call-with-ncco.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'dotenv'
 
 Dotenv.load
@@ -8,21 +10,21 @@ NEXMO_APPLICATION_PRIVATE_KEY_PATH = ENV['NEXMO_APPLICATION_PRIVATE_KEY_PATH']
 require 'nexmo'
 
 client = Nexmo::Client.new(
-    application_id: NEXMO_APPLICATION_ID,
-    private_key: File.read(NEXMO_APPLICATION_PRIVATE_KEY_PATH)
+  application_id: NEXMO_APPLICATION_ID,
+  private_key: File.read(NEXMO_APPLICATION_PRIVATE_KEY_PATH)
 )
 
 client.calls.create(
-    to: [{
-        type: 'phone',
-        number: RECIPIENT_NUMBER
-    }],
-    from: {
-        type: 'phone',
-        number: NEXMO_NUMBER
-    },
-    ncco: [
-            'action' => 'talk',
-            'text' => 'This is a text to speech call from Nexmo'
-        ] 
+  to: [{
+    type: 'phone',
+    number: RECIPIENT_NUMBER
+  }],
+  from: {
+    type: 'phone',
+    number: NEXMO_NUMBER
+  },
+  ncco: [
+    'action' => 'talk',
+    'text' => 'This is a text to speech call from Nexmo'
+  ]
 )
--- voice/track-ncco-progress.rb
+++ voice/track-ncco-progress.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'sinatra'
 require 'sinatra/multi_route'
 require 'json'
@@ -5,40 +7,40 @@ require 'json'
 YOUR_URL = 'put your externally accessible url here'
 
 before do
-    content_type :json
+  content_type :json
 end
 
 route :get, :post, '/webhooks/answer' do
+  [
     [
-        [
-            'action' => 'talk',
-            'text' => 'Thanks for calling the notification line'
-        ],
-        [
-            'action' => 'notify',
-            'payload' => ['foo' => 'bar'],
-            'eventUrl' => ["#{YOUR_URL}/webhooks/notifications"]
-        ],
-        [
-            'action' => 'talk',
-            'text' => 'You will never hear me as the notification URL will return an NCCO'
-        ]
-    ].to_json
+      'action' => 'talk',
+      'text' => 'Thanks for calling the notification line'
+    ],
+    [
+      'action' => 'notify',
+      'payload' => ['foo' => 'bar'],
+      'eventUrl' => ["#{YOUR_URL}/webhooks/notifications"]
+    ],
+    [
+      'action' => 'talk',
+      'text' => 'You will never hear me as the notification URL will return an NCCO'
+    ]
+  ].to_json
 end
 
 route :get, :post, '/webhooks/notifications' do
-    puts params
+  puts params
+  [
     [
-        [
-            'action' => 'talk',
-            'text' => 'Your notification has been received, loud and clear'
-        ]
-    ].to_json
+      'action' => 'talk',
+      'text' => 'Your notification has been received, loud and clear'
+    ]
+  ].to_json
 end
 
 route :get, :post, 'webhooks/event' do
-    puts params
-    halt 204
+  puts params
+  halt 204
 end
 
 set :port, 3000
