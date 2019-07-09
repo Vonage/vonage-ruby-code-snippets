9d6fc7ddc14e0c85d746b4a2575ad5c1b9965ac8
benhayehudi <bengreenberg@gmail.com>
2019-07-09 14:16:27 +0300

    add redact ruby snippet

---
+++ redact/redact.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'dotenv'
+Dotenv.load
+
+NEXMO_API_KEY = ENV['NEXMO_API_KEY']
+NEXMO_API_SECRET = ENV['NEXMO_API_SECRET']
+NEXMO_REDACT_ID = ENV['REDACT_ID']
+
+NEXMO_REDACT_TYPE = ARGV[0]
+if PRODUCT.empty?
+  puts 'Please supply the product type'
+  exit
+end
+
+require 'nexmo'
+
+client = Nexmo::Client.new(
+  api_key: NEXMO_API_KEY,
+  api_secret: NEXMO_API_SECRET
+)
+
+client.redact.transaction(
+  id: NEXMO_REDACT_ID,
+  product: NEXMO_REDACT_TYPE
+)
