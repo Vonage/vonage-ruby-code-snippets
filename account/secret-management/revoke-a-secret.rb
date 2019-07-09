d23d20d95c5e078b304f2c416dd5b3f803447871
Ben Greenberg <benhayehudi@users.noreply.github.com>
2019-07-09 18:29:41 +0300

    Add Account Secret Management Ruby Code Snippets (#44)
    * add create a secret ruby snippet
    * add fetch a secret ruby snippet
    * added revoke a secret ruby snippet
    * added list all secrets ruby snippet
    * added client instantiation to examples, updated nexmo and printing meaningful results for list all secrets snippet

---
+++ account/secret-management/revoke-a-secret.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'dotenv'
+
+Dotenv.load
+
+NEXMO_API_KEY = ENV['NEXMO_API_KEY']
+NEXMO_API_SECRET = ENV['NEXMO_API_SECRET']
+
+NEXMO_SECRET_ID = ENV['NEXMO_SECRET_ID']
+
+require 'nexmo'
+
+client = Nexmo::Client.new(
+  api_key: NEXMO_API_KEY,
+  api_secret: NEXMO_API_SECRET
+)
+
+begin
+  response = client.secrets.revoke(NEXMO_SECRET_ID)
+  puts 'OK' if response == :no_content
+rescue StandardError => e
+  puts e.message
+end
