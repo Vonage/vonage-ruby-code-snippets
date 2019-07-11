2d312dde981e48d7ff078ca09fe3a2b31f2f2524
benhayehudi <bengreenberg@gmail.com>
2019-07-11 07:24:19 +0300

    add code snippet for transferring a call with inline ncco

---
+++ voice/transfer-a-call-ncco.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'json'
+require 'dotenv'
+
+Dotenv.load
+
+NEXMO_APPLICATION_ID = ENV['NEXMO_APPLICATION_ID']
+NEXMO_APPLICATION_PRIVATE_KEY_PATH = ENV['NEXMO_APPLICATION_PRIVATE_KEY_PATH']
+
+require 'nexmo'
+
+client = Nexmo::Client.new(
+  application_id: NEXMO_APPLICATION_ID,
+  private_key: File.read(NEXMO_APPLICATION_PRIVATE_KEY_PATH)
+)
+
+ncco = {
+  "type": 'ncco',
+  "ncco": [
+    "action": 'talk',
+    "text": 'This is a transfer action using an inline NCCO'
+  ]
+}
+
+response = client.calls.transfer(UUID, destination: ncco)
+
+puts response.inspect
