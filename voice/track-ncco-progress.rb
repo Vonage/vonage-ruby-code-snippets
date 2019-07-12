4c5092e4f753dd655e73355f5353c5c8573cb9a0
benhayehudi <bengreenberg@gmail.com>
2019-07-12 10:23:19 +0300

    fixed ncco output in routes

--- voice/track-ncco-progress.rb
+++ voice/track-ncco-progress.rb
@@ -10,29 +10,29 @@ end
 
 route :get, :post, '/webhooks/answer' do
   [
-    [
+    {
       'action' => 'talk',
       'text' => 'Thanks for calling the notification line'
-    ],
-    [
+    },
+    {
       'action' => 'notify',
       'payload' => ['foo' => 'bar'],
       'eventUrl' => ["#{request.base_url}/webhooks/notification"]
-    ],
-    [
+    },
+    {
       'action' => 'talk',
       'text' => 'You will never hear me as the notification URL will return an NCCO'
-    ]
+    }
   ].to_json
 end
 
 route :get, :post, '/webhooks/notification' do
   puts params
   [
-    [
+    {
       'action' => 'talk',
       'text' => 'Your notification has been received, loud and clear'
-    ]
+    }
   ].to_json
 end
 
