03eef30f33c43e1c69526b95f1526f8014a33976
benhayehudi <bengreenberg@gmail.com>
2019-08-06 15:09:07 -0400

    remove byebug

--- application/list-applications.rb
+++ application/list-applications.rb
@@ -8,11 +8,12 @@ NEXMO_API_KEY = ENV['NEXMO_API_KEY']
 NEXMO_API_SECRET = ENV['NEXMO_API_SECRET']
 
 require 'nexmo'
-require 'byebug'
+
 client = Nexmo::Client.new(
   api_key: NEXMO_API_KEY,
   api_secret: NEXMO_API_SECRET
 )
+
 response = client.applications_v2.list
 
 response._embedded.applications.each do |application|
