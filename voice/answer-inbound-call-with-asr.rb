341f247976b3a6b262f13fd325dcaefe60e37ae3
benhayehudi <bengreenberg@gmail.com>
2020-04-20 10:13:24 +0300

    add ASR Ruby snippet
    remove commented line

---
+++ voice/answer-inbound-call-with-asr.rb
@@ -0,0 +1,40 @@
+require 'sinatra'
+require 'sinatra/multi_route'
+require 'rack/contrib'
+
+use Rack::JSONBodyParser
+
+before do
+  content_type :json
+end
+
+route :get, :post, '/webhooks/answer' do
+  [
+    {
+      action: 'talk',
+      text: 'Please say something'
+    },
+    {
+      action: 'input',
+      eventUrl: ["#{request.base_url}/webhooks/asr"],
+      speech: {
+        endOnSilence: 1,
+        uuid: [params[:uuid]],
+        language: 'en-US'
+      }
+    }
+  ].to_json
+end
+
+route :post, '/webhooks/asr' do
+  [{
+    action: 'talk',
+    text: "You said #{params["speech"]["results"][0]["text"]}"
+  }].to_json
+end
+
+route :post, '/webhooks/event' do
+  puts params
+end
+
+set :port, 3000
