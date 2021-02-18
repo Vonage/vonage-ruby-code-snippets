38ae8ceeeeda951b34e7b599f21b6bb461963827
bencgreenberg <bengreenberg@gmail.com>
2021-02-18 11:12:53 +0200

    add info on jwt ruby usage

--- jwt/decode-incoming-jwt.rb
+++ jwt/decode-incoming-jwt.rb
@@ -2,5 +2,7 @@ require 'jwt'
 
 SIGNATURE = ARGV[0] || ENV['SIGNATURE']
 
+# The JWT Ruby spec requires nil and false values provided, respectively, for the two optional parameters
+# See: https://github.com/jwt/ruby-jwt#algorithms-and-usage
 decoded = JWT.decode(SIGNATURE, nil, false)
 puts decoded
