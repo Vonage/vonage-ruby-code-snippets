a70f47cfbdbe62139d9424d7d28a6fd695da05e0
superchilled <karl@superchilled.co.uk>
2022-06-01 12:58:46 +0100

    Adding Facebook Messenger code snippets for Messages API

---
+++ messages/messenger/send-audio.rb
@@ -0,0 +1,25 @@
+require 'dotenv/load'
+require 'vonage'
+
+VONAGE_APPLICATION_ID = ENV['VONAGE_APPLICATION_ID']
+VONAGE_APPLICATION_PRIVATE_KEY_PATH = ENV['VONAGE_APPLICATION_PRIVATE_KEY_PATH']
+VONAGE_FB_SENDER_ID = ENV['VONAGE_FB_SENDER_ID']
+FB_RECIPIENT_ID = ENV['FB_RECIPIENT_ID']
+
+client = Vonage::Client.new(
+  application_id: VONAGE_APPLICATION_ID,
+  private_key: File.read(VONAGE_APPLICATION_PRIVATE_KEY_PATH)
+)
+
+message = Vonage::Messaging::Message.messenger(
+  type: 'audio',
+  message: {
+    url: "https://example.com/audio.mp3"
+  }
+)
+
+client.messaging.send(
+  from: VONAGE_FB_SENDER_ID,
+  to: FB_RECIPIENT_ID,
+  **message
+)
