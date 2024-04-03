974d7699f3efb9bfc3fc0ea2bb68ee9f91707d4e
superchilled <karl@superchilled.co.uk>
2024-04-03 10:18:59 +0100

    Adding Messages Sandbox WhatsApp snippet

---
+++ messages/whatsapp/send-text-sandbox.rb
@@ -0,0 +1,25 @@
+require 'dotenv/load'
+require 'vonage'
+
+VONAGE_APPLICATION_ID = ENV['VONAGE_APPLICATION_ID']
+VONAGE_APPLICATION_PRIVATE_KEY_PATH = ENV['VONAGE_APPLICATION_PRIVATE_KEY_PATH']
+MESSAGES_SANDBOX_URI= ENV['MESSAGES_SANDBOX_URI']
+MESSAGES_SANDBOX_WHATSAPP_NUMBER = ENV['MESSAGES_SANDBOX_WHATSAPP_NUMBER']
+MESSAGES_SANDBOX_ALLOW_LISTED_TO_NUMBER = ENV['MESSAGES_SANDBOX_ALLOW_LISTED_TO_NUMBER']
+
+client = Vonage::Client.new(
+  application_id: VONAGE_APPLICATION_ID,
+  private_key: File.read(VONAGE_APPLICATION_PRIVATE_KEY_PATH),
+  api_host: MESSAGES_SANDBOX_URI
+)
+
+message = Vonage::Messaging::Message.whatsapp(
+  type: 'text',
+  message: "This is a WhatsApp Message text message sent using the Messages API via the Messages Sandbox"
+)
+
+client.messaging.send(
+  from: MESSAGES_SANDBOX_WHATSAPP_NUMBER,
+  to: MESSAGES_SANDBOX_ALLOW_LISTED_TO_NUMBER,
+  **message
+)
