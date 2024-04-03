6b298012eac09763d997725f2470d28ffb374111
superchilled <karl@superchilled.co.uk>
2024-04-03 11:35:25 +0100

    Changing folder structure and variable names

---
+++ messages/sandbox/messenger/send-text.rb
@@ -0,0 +1,25 @@
+require 'dotenv/load'
+require 'vonage'
+
+VONAGE_APPLICATION_ID = ENV['VONAGE_APPLICATION_ID']
+VONAGE_APPLICATION_PRIVATE_KEY_PATH = ENV['VONAGE_APPLICATION_PRIVATE_KEY_PATH']
+MESSAGES_SANDBOX_URI= ENV['MESSAGES_SANDBOX_URI']
+MESSAGES_SANDBOX_FB_ID = ENV['MESSAGES_SANDBOX_FB_ID']
+MESSAGES_SANDBOX_ALLOW_LISTED_FB_RECIPIENT_ID = ENV['MESSAGES_SANDBOX_ALLOW_LISTED_FB_RECIPIENT_ID']
+
+client = Vonage::Client.new(
+  application_id: VONAGE_APPLICATION_ID,
+  private_key: File.read(VONAGE_APPLICATION_PRIVATE_KEY_PATH),
+  api_host: MESSAGES_SANDBOX_URI
+)
+
+message = Vonage::Messaging::Message.messenger(
+  type: 'text',
+  message: "This is a Facebook Messenger text message sent using the Messages API via the Messages Sandbox"
+)
+
+client.messaging.send(
+  from: MESSAGES_SANDBOX_FB_ID,
+  to: MESSAGES_SANDBOX_ALLOW_LISTED_FB_RECIPIENT_ID,
+  **message
+)
---
+++ messages/sandbox/viber/send-text.rb
@@ -0,0 +1,25 @@
+require 'dotenv/load'
+require 'vonage'
+
+VONAGE_APPLICATION_ID = ENV['VONAGE_APPLICATION_ID']
+VONAGE_APPLICATION_PRIVATE_KEY_PATH = ENV['VONAGE_APPLICATION_PRIVATE_KEY_PATH']
+MESSAGES_SANDBOX_URI= ENV['MESSAGES_SANDBOX_URI']
+MESSAGES_SANDBOX_VIBER_SERVICE_ID = ENV['MESSAGES_SANDBOX_VIBER_SERVICE_ID']
+MESSAGES_SANDBOX_ALLOW_LISTED_TO_NUMBER = ENV['MESSAGES_SANDBOX_ALLOW_LISTED_TO_NUMBER']
+
+client = Vonage::Client.new(
+  application_id: VONAGE_APPLICATION_ID,
+  private_key: File.read(VONAGE_APPLICATION_PRIVATE_KEY_PATH),
+  api_host: MESSAGES_SANDBOX_URI
+)
+
+message = Vonage::Messaging::Message.viber(
+  type: 'text',
+  message: "This is a Viber Service Message text message sent using the Messages API"
+)
+
+client.messaging.send(
+  from: MESSAGES_SANDBOX_VIBER_SERVICE_ID,
+  to: MESSAGES_SANDBOX_ALLOW_LISTED_TO_NUMBER,
+  **message
+)
