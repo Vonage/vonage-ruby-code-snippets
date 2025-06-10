f849d6e084bf7d0bab8cfd598e4fa39fd6fbb07e
superchilled <karl@superchilled.co.uk>
2025-06-10 14:38:36 +0100

    Updating Secrets snippets

--- account/secret-management/fetch-a-secret.rb
+++ account/secret-management/fetch-a-secret.rb
@@ -5,13 +5,11 @@ require 'vonage'
 
 VONAGE_API_KEY = ENV['VONAGE_API_KEY']
 VONAGE_API_SECRET = ENV['VONAGE_API_SECRET']
-
-VONAGE_SECRET_ID = ENV['VONAGE_SECRET_ID']
+ACCOUNT_SECRET_ID = ENV['ACCOUNT_SECRET_ID']
 
 client = Vonage::Client.new(
   api_key: VONAGE_API_KEY,
   api_secret: VONAGE_API_SECRET
 )
-result = client.secrets.get(VONAGE_SECRET_ID)
 
-puts "ID: #{result.id} created on #{result.created_at}"
+response = client.secrets.get(ACCOUNT_SECRET_ID)
